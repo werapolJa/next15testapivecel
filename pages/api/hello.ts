1b5eec7b892ca86796b2b70fea1f8c93a30e4643
werapol <wesakaber@gmail.com>
2025-02-07 20:37:39 +0700

    vercel

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,3 +1,4 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import connectionPool from "@/utils/db";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -5,31 +6,24 @@ type User = {
   user_id: number;
   full_name: string;
   phone: string;
+  id_number?: string;
+  image?: string;
+  birthdate?: string;
+  status: string;
+  create_at: string;
+  update_at: string;
 };
 
 type Data = {
   data?: User[];
-  error?: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  // Check if NEXT_PUBLIC_SUPABASE_URL is set
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-
-  if (!supabaseUrl) {
-    // If the environment variable is not set, return an error response
-    return res.status(500).json({ error: "Supabase URL is not set in environment variables." });
-  }
-
   if (req.method === "GET") {
-    try {
-      const result = await connectionPool.query("SELECT * FROM posts");
-      res.status(200).json({ data: result.rows });
-    } catch  {
-      res.status(500).json({ error: "An error occurred while querying the database." });
-    }
+    const result = await connectionPool.query("select * from posts");
+    res.status(200).json({ data: result.rows });
   }
 }
